fe3847e6f24de49b7629af2be2d2a5f937ae5083
adolfvitor <tong.shen@17dianjia.com>
2023-08-03 09:05:10 +0800

    clear all files

--- README.md
+++
@@ -1,4 +0,0 @@
-# echarts vue3
-
-# eviroment
-## node 16.14.0 
--- vue3-echart-digital-screen/src/api/index.ts
+++
@@ -1,6 +0,0 @@
-import request from '@/config/axios'
-
-// 设备echart图表1
-export const deviceEchartCountApi = () => {
-    return request.get({ url: `/customer/stat/orgDevice` })
-  }
