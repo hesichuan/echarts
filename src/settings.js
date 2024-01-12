390ee7b138a02e60f23d4e8b5fc2fdddae469722
adolfvitor <tong.shen@17dianjia.com>
2024-01-12 18:54:48 +0800

    update: header backgroud

--- src/settings.js
+++ src/settings.js
@@ -3,17 +3,17 @@ export default {
    * @type {string} *
    * @description 系统标题title
    */
-  systemTitle: "设备共享平台",
+  systemTitle: '中油工程设备',
 
   /**
    * @type {string} *
    * @description 版权公司
    */
-  copyrightCompany: "xxx公司",
+  copyrightCompany: 'xxx公司',
 
   /**
    * @type {number} *
    * @description dev环境下项目运行端口
    */
-  webPort: 9999,
-};
+  webPort: 9999
+}
